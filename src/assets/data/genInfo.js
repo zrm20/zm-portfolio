c31d62214b28defc237a3515b9d2cd72611a73b3
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2022-09-06 11:16:20 -0500

    add genInfo section

---
+++ src/assets/data/genInfo.js
@@ -0,0 +1,37 @@
+import { faApple, faWindows } from "@fortawesome/free-brands-svg-icons";
+import { faGraduationCap, faHouseUser, faIdCard, faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
+
+const genInfo = [
+  {
+    title: 'Age',
+    value: 30,
+    icon: faIdCard
+  },
+  {
+    title: 'Hometown',
+    value: 'Marshall, MN',
+    icon: faMapLocationDot
+  },
+  {
+    title: 'Current Home',
+    value: 'Pleasant View, TN',
+    icon: faHouseUser
+  },
+  {
+    title: 'Education',
+    value: 'Belmont University 2014',
+    icon: faGraduationCap
+  },
+  {
+    title: 'Mac',
+    value: 'Proficient',
+    icon: faApple
+  },
+  {
+    title: 'Windows',
+    value: 'Proficient',
+    icon: faWindows
+  }
+];
+
+export default genInfo;
--- src/components/resume/GeneralInfo.js
+++ src/components/resume/GeneralInfo.js
@@ -1,16 +1,42 @@
-import { Container, Typography } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Container, Typography, Box, Grid } from '@mui/material';
+import genInfo from '../../assets/data/genInfo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
 export default function GeneralInfo({ attributes = [] }) {
   return (
-    <Container >
-      <Typography variant='h6'>General Info</Typography>
-      <div>
+    <Container sx={{ mb: 1 }}>
+      <Typography variant='h6' gutterBottom>General Info</Typography>
+      <Grid container justifyContent='space-evenly'>
         {
-
+          genInfo.map(el => (
+            <Grid item key={el.title} sx={styles.infoItem}>
+              <Typography sx={styles.title}>{el.title}</Typography>
+              <FontAwesomeIcon icon={el.icon || faCircleInfo} />
+              <Typography variant='subtitle2' sx={styles.value}>{el.value}</Typography>
+            </Grid>
+          ))
         }
-      </div>
+      </Grid>
     </Container>
   )
+};
+
+const styles = {
+  infoItem: {
+    '& svg': { 
+      fontSize: '1.5rem', 
+      m: 1,
+      color: 'primary.main'
+    }
+  },
+  title: {
+    fontSize: '.6rem', 
+    color: 'textSecondary'
+  },
+  value: {
+    color: 'textSecondary'
+  }
 };
 

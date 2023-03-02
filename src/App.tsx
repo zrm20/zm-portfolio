8e953a322458ea1789b3c9557e9f9c881a24a468
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-03-02 08:57:19 -0600

    update App to use FadeIn and Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import theme from './styles/theme';
-import Layout from './components/Layout';
+import { Layout } from './components/partials';
 import Home from './components/Home';
 import DevRouter from './components/dev/DevRouter';
 import AudioRouter from './components/audio/AudioRouter';
 import IntroBackdrop from './components/atoms/IntroBackdrop';
 import { AnimatePresence, motion } from 'framer-motion';
+import { FadeIn } from './components/animations';
 
 export default function App() {
   const [backdropOpen, setBackdropOpen] = useState(true);
@@ -30,21 +31,16 @@ export default function App() {
             backdropOpen ?
               <IntroBackdrop />
               :
-              <motion.div
-                style={styles.root}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-              >
+              <FadeIn style={styles.root}>
                 <Layout>
-                  <Routes>
+                  {/* <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/dev/*' element={<DevRouter />} />
                     <Route path='/audio/*' element={<AudioRouter />} />
                     <Route path='/*' element={<Home />} />
-                  </Routes>
+                  </Routes> */}
                 </Layout>
-              </motion.div>
+              </FadeIn>
           }
         </AnimatePresence>
 

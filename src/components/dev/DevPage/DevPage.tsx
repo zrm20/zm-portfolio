331f4f79b679b79a5efb837d4b9333b2f62fd634
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-03-02 21:29:55 -0600

    add title updates to dev and home

--- src/components/dev/DevPage/DevPage.tsx
+++ src/components/dev/DevPage/DevPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 
 import useStyles from "./DevPage.styles";
+import { useUpdateTitle } from "../../../hooks";
 
 interface DevPageProps {
 
@@ -9,6 +10,7 @@ interface DevPageProps {
 
 export default function DevPage(props: DevPageProps): JSX.Element {
   const styles = useStyles();
+  useUpdateTitle("ZM - Software")
 
   return (
     <Box sx={styles.root} >
--- src/components/home/Home/Home.tsx
+++ src/components/home/Home/Home.tsx
@@ -3,12 +3,14 @@ import { Grid } from "@mui/material"
 
 import useStyles from "./Home.styles";
 import AboutCard from "../AboutCard/AboutCard";
+import { useUpdateTitle } from "../../../hooks";
 
 interface HomeProps {
 };
 
 export default function Home(props: HomeProps): JSX.Element {
   const styles = useStyles();
+  useUpdateTitle("Zach McCoy")
 
   return (
     <Grid container sx={styles.root}>

664da11f3e8f02efa663d110e93a35470769862f
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-03-04 17:30:25 -0500

    move images around

--- src/database/Projects.ts
+++ src/database/Projects.ts
@@ -1,4 +1,8 @@
-import trailPackProLogo from "../assets/images/packplannerlogo.png";
+import trailPackProLogo from "../assets/images/projectImages/packplannerlogo.png";
+import nomiLogo from "../assets/images/experienceImages/nomi_logo.png";
+import mealsToGoLogo from "../assets/images/projectImages/meals_to_go_logo.png";
+import zmLogo from "../assets/images/zm_logo_orange_white_name.PNG";
+import zmPortfolio1 from "..//assets/images/projectImages/zm_portfolio_1.jpg";
 
 const projects: Project[] = [
   {
@@ -22,7 +26,8 @@ const projects: Project[] = [
     description: "This was my first attempt at a solo project. As hobbyist backpacker, I found myself always writing out my inventory and weights for each backpacking trip. Everyone has their own target weight and I thought it would be nice to be able to know that weight, without having to pack all my gear up first. While apps that do this already existed, it seemed like a great oppertunity to take on my first solo challenge.\nWith that, Trail Pack Pro was born - A React Native application that allows you to store your inventory of gear along with the weight. You can add that gear to your pack and view your total backpack weight. It also has features like a chart for viewing weight by category.\nMy favorite feature though, and the feature that sets this apart from other apps, is the water weight. Trail Pack Pro will automatically calculate the weight of the water you are carrying based on the total volume available by the items in your pack. You can adjust a slider to see how your weight will change as you consume water.\nWhile a TestFlight beta was released, I have not officially released Trail Pack Pro to the app store, however that is coming!",
     website: "https://www.zachrmccoy.com",
     skills: ["javascript", "typescript", "firebase"],
-    images: []
+    images: [zmPortfolio1],
+    logo: zmLogo
   },
   {
     id: "nomi_web_app",
@@ -31,7 +36,8 @@ const projects: Project[] = [
     subtitle: "React web app for company landing page and user account management",
     description: "At Nomi Travel, my primary responsibility was to build the web based front-end. Nomi is a SMS based service that personalizes local search. Starting in Nashille, TN users could text Nomi with \"Where can I get coffee\" and rather than sifting through traditional chain companies, they would receive a local and personal recommendation.\nNomi's website was built in React and is primarily a landing page for new users and company information. Users can sign up for the service with just a phone number and a verification code and recieve their first recommendation in just minutes.\nThe backend of Nomi involved connecting to multiple external API's such as Twilio, Google and others.",
     skills: ["react", "redux", "javascipt", "node", "express", "mongo_db"],
-    images: []
+    images: [],
+    logo: nomiLogo
   },
   {
     id: "nomi_activity_manager",
@@ -40,7 +46,8 @@ const projects: Project[] = [
     subtitle: "A web dashboard for Nomi employees offering database CRUD operations",
     description: "At Nomi, I also was responsible for building an internal app for database management. A major part of Nomi is the refined list of restaraunts and nightlife activities. It was critical for our team that all members could add and modify these places in our database, without needing to understand MongoDB or any code.\nThe database managagement app offered full CRUD functionality for multiple data resources across three environments using a full MERN stack.",
     skills: ["react", "redux", "javascipt", "node", "express", "mongo_db"],
-    images: []
+    images: [],
+    logo: nomiLogo
   },
   {
     id: "nomi_mobile_app",
@@ -49,7 +56,8 @@ const projects: Project[] = [
     subtitle: "A mobile app for consumers offering personalized restaurant and entertainment suggestions",
     description: "The Nomi mobile app is a cross platform mobile app with a full dashboard of personalized recommendations. Users register and participate in a 3 question personality survey and then recieve restaurant recommendations tailored for them.",
     skills: ["react_native", "firebase", "javascript"],
-    images: []
+    images: [],
+    logo: nomiLogo
   },
   {
     id: "meals_to_go",
@@ -59,7 +67,8 @@ const projects: Project[] = [
     description: "As part of a React Native Udemy course, this code along prooject built a full mobile app for restaurant search by location. Using Firebase authentication users could sign up and search for places and favorite them.\nWhile the end result is not connected to the Google Places API, a mock API call is made to return placeholder data for demo purposes. User account data is also stored locally on the device for offline use.\nWhile the UI of this app is exactly the same as the instructors app in the course, I took several liberties in the code architecture as well as the use of custom hooks.",
     skills: ["react_native", "firebase", "javascript"],
     images: [],
-    repo: "https://github.com/zrm20/MealsToGo"
+    repo: "https://github.com/zrm20/MealsToGo",
+    logo: mealsToGoLogo
   }
 ];
 

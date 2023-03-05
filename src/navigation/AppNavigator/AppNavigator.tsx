6feb0233fbec90f6b13930707443159cb3a328bc
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-03-04 19:52:13 -0500

    refactor navigation

--- src/navigation/AppNavigator/AppNavigator.tsx
+++ src/navigation/AppNavigator/AppNavigator.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { DevPage } from "../../components/dev";
 
 import { Home } from "../../components/home";
-import DevNavigator from "../DevNavigator/DevNavigator";
 
 export default function AppNavigator(): JSX.Element {
 
   return (
     <Routes>
-      <Route path="/audio" />
-      <Route path="/dev/*" element={<DevNavigator />} />
+      <Route path="/audio/*" />
+      <Route path="/dev/*" element={<DevPage />} />
+      <Route path="/projects" />
+      <Route path="/projects/:id" />
+      <Route path="/experiences" />
+      <Route path="/experiences/:id" />
+      <Route path="/education" />
+      <Route path="/education/:id" />
+      <Route path="/skills" />
+      <Route path="/skills/:id" />
       <Route path="/" element={<Home />} />
     </Routes>
   );

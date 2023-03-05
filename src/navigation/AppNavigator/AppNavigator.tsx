a5a0a8a730936bf111c2bf24012e837bb5043b07
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-03-05 11:13:10 -0600

    add resume to routes

--- src/navigation/AppNavigator/AppNavigator.tsx
+++ src/navigation/AppNavigator/AppNavigator.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { DevPage } from "../../components/dev";
+import DevResumePage from "../../components/dev/DevResumePage";
 
 import { Home } from "../../components/home";
 import { useScrollToSection } from "../../hooks";
@@ -12,6 +13,7 @@ export default function AppNavigator(): JSX.Element {
     <Routes>
       <Route path="/audio/*" />
       <Route path="/dev/*" element={<DevPage />} />
+      <Route path="/resume" element={<DevResumePage />} />
       <Route path="/projects" />
       <Route path="/projects/:id" />
       <Route path="/experiences" />

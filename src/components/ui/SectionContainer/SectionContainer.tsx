288e1d2b12b6add9830962dac4814fb4fc215cc2
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-03-06 08:56:18 -0600

    add style and sx props for flexibiity

--- src/components/ui/SectionContainer/SectionContainer.tsx
+++ src/components/ui/SectionContainer/SectionContainer.tsx
@@ -1,20 +1,23 @@
-import React from "react";
-import { Box, Divider, Typography } from "@mui/material";
+import React, { CSSProperties } from "react";
+import { Box, Divider, SxProps, Typography } from "@mui/material";
 
 import useStyles from "./SectionContainer.styles";
 
 interface SectionContainerProps {
   title: string;
   id: string;
-  children: JSX.Element | JSX.Element[]
+  children: JSX.Element | JSX.Element[],
+  style?: CSSProperties
+  sx?: SxProps
+  titleSx?: SxProps
 };
 
 export default function SectionContainer(props: SectionContainerProps): JSX.Element {
   const styles = useStyles();
 
   return (
-    <Box sx={styles.root} component="section" id={props.id}>
-      <Typography sx={styles.title} variant="h4">{props.title}</Typography>
+    <Box sx={props.sx || styles.root} component="section" id={props.id} style={props.style}>
+      <Typography sx={props.titleSx || styles.title} variant="h4">{props.title}</Typography>
       <Divider />
       <Box sx={styles.container}>
         {props.children}

d4c891c57f4eaf78d89687955dd52b4e2103cfd2
Zach McCoy <71274676+zrm20@users.noreply.github.com>
2023-04-02 08:52:45 -0500

    add contact constants and update url constants

---
+++ src/constants/contact.ts
@@ -0,0 +1,5 @@
+export const PHONE = "(507) 829-2069";
+export const PHONE_LINK = "tel:" + PHONE;
+
+export const EMAIL = "zach@zachrmccoy.com";
+export const EMAIL_LINK = "mailto:" + EMAIL;
--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,3 +1,8 @@
-export const GITHUB_URL = "https://www.github.com/zrm20";
+export const GITHUB_PATH = "/zrm20";
+export const GITHUB_URL = "https://www.github.com" + GITHUB_PATH;
 
-export const LINKEDIN_URL = "https://www.linkedin.com/zachmccoy";
+export const LINKEDIN_PATH = "/in/zachmccoy";
+export const LINKEDIN_URL = "https://www.linkedin.com" + LINKEDIN_PATH;
+
+export const PORTFOLIO_DOMAIN = "zachrmccoy.com";
+export const PORTFOLIO_URL = "https://www." + PORTFOLIO_DOMAIN;
